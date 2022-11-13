39f03efaf84eea0aedf41cb12ba1f9ba0f3bbd1d
dbarros1979 <daniel.barros@genesis.global>
2022-11-13 22:15:25 +0000

    Exercise 2.3

--- client/web/src/routes/trade/trade.template.ts
+++ client/web/src/routes/trade/trade.template.ts
@@ -23,5 +23,45 @@ export const TradeTemplate = html<Trade>`
       `)}
     </zero-select>
   </div>
-<//zero-card>
+  <div>
+    <zero-text-field required :value=${sync(x=> x.quantity)}>Quantity</zero-text-field>
+    <zero-text-field :value=${sync(x=> x.price)}>Price</zero-text-field>
+  </div>
+  <div>
+    <zero-text-field :value=${sync(x=> x.symbol)}>Symbol</zero-text-field>
+  </div>
+  <div>
+    <span>Direction</span>
+    <zero-select :value=${sync(x=> x.direction)}>
+      ${repeat(x => x.directionOptions, html`
+        <zero-option value=${x => x.value}>${x => x.label}</zero-option>
+      `)}
+    </zero-select>
+  </div>
+  <div>
+    <span>Status</span>
+    <zero-select :value=${sync(x=> x.status)}>
+      ${repeat(x => x.statusOptions, html`
+        <zero-option value=${x => x.value}>${x => x.label}</zero-option>
+      `)}
+    </zero-select>
+  </div>
+  <div>
+    <zero-button @click=${x=> x.insertTrade()}>Add Trade</zero-button>
+  </div>
+  <zero-grid-pro rowHeight="20" persist-column-state-key='trade-grid-settings'>
+      <grid-pro-genesis-datasource
+          resourceName="ALL_TRADES"
+          orderBy="TRADE_ID">
+      </grid-pro-genesis-datasource>
+      ${repeat(() => tradeColumnDefs, html`
+      <grid-pro-column :definition="${x => x}" />
+      `)}
+  </zero-grid-pro>
+  ${when(x => x.serverResponse, html`
+  <span>${x=> x.serverResponse.MESSAGE_TYPE == 'EVENT_ACK' ?
+    'Successfully added trade' : 'Something went wrong'}
+  </span>
+  `)}
+</zero-card>
 `;
--- client/web/src/routes/trade/trade.ts
+++ client/web/src/routes/trade/trade.ts
@@ -23,6 +23,8 @@ export class Trade extends FASTElement {
 
     @observable public allCounterparties: Array<{value: string, label: string}>; //add this property
     @observable public allInstruments: Array<{value: string, label: string}>; //add this property
+    @observable public directionOptions: Array<{value: string, label: string}>; //add this property
+    @observable public statusOptions: Array<{value: string, label: string}>; //add this property
 
     @observable public serverResponse;
 
@@ -40,10 +42,16 @@ export class Trade extends FASTElement {
         const msgI = await this.connect.snapshot('ALL_INSTRUMENTS'); //get a snapshot of data from ALL_INTRUMENTS data server
         this.allInstruments = msgI.ROW?.map(instrument => ({
           value: instrument.INSTRUMENT_ID, label: instrument.INSTRUMENT_NAME}));
+
+        const metadata = await this.connect.getMetadata('ALL_TRADES');
+        const directionField = metadata.FIELD?.find(field => field.NAME == 'DIRECTION');
+        this.directionOptions = Array.from(directionField.VALID_VALUES).map(v => ({value: v, label: v}));
+        const statusField = metadata.FIELD?.find(field => field.NAME == 'TRADE_STATUS');
+        this.statusOptions = Array.from(statusField.VALID_VALUES).map(v => ({value: v, label: v}));
     }    
 
     public async insertTrade() {
-        this.serverResponse = await this.connect.commitEvent('EVENT_ORDER_INSERT', {
+        this.serverResponse = await this.connect.commitEvent('EVENT_TRADE_INSERT', {
           DETAILS: {
             TRADE_ID: Date.now(), 
             COUNTERPARTY_ID: this.counterparty,
@@ -52,7 +60,8 @@ export class Trade extends FASTElement {
             QUANTITY: this.quantity,
             PRICE: this.price,
             DIRECTION: this.direction,
-            TRADE_STATUS: this.status
+            TRADE_STATUS: this.status,
+            TRADE_DATE: Date.now()
           },
         });
         console.log(this.serverResponse);
