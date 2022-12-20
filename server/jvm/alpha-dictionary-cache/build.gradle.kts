24a590ac11bf04b02ae46aca7a781eddb2d924fd
sebastiangalvao <89458693+sebastiangalvao@users.noreply.github.com>
2022-12-20 20:45:10 -0300

    sync with seed

--- server/jvm/alpha-dictionary-cache/build.gradle.kts
+++ server/jvm/alpha-dictionary-cache/build.gradle.kts
@@ -1,7 +1,14 @@
 
 // Add your genesis config dependencies here
 dependencies {
+    implementation(project(":alpha-dictionary-cache:alpha-generated-dao"))
+    implementation(project(":alpha-dictionary-cache:alpha-generated-fields"))
+    implementation(project(":alpha-dictionary-cache:alpha-generated-hft"))
+    implementation(project(":alpha-dictionary-cache:alpha-generated-sysdef"))
+    implementation(project(":alpha-dictionary-cache:alpha-generated-view"))
 
+    implementation("global.genesis:auth-config:${properties["authVersion"]}")
+    implementation("global.genesis:reporting-config:${properties["reportingVersion"]}")
 }
 
 description = "alpha-dictionary-cache"
